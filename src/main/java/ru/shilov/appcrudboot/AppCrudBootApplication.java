8cfd71ba66a7cd74a4321127b7ac11fb3d964791
Vladislav Shilov <Shilovvlad@mail.ru>
2022-12-19 12:10:55 +0300

    init commit

---
+++ src/main/java/ru/shilov/appcrudboot/AppCrudBootApplication.java
@@ -0,0 +1,13 @@
+package ru.shilov.appcrudboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrudBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCrudBootApplication.class, args);
+    }
+
+}
